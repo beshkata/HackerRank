6a16b37351dc2d0f230f4579682b5a717140f469
beshkata <beshkata141@gmail.com>
2021-01-08 14:49:38 +0200

    Added solutions for "Birthday Cake Candles" and "Diagonal Difference" problems

---
+++ Problem-Solving-Easy-Difficulty/Birthday-Cake-Candles/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Birthday_Cake_Candles
+{
+    //https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+    class Result
+    {
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int result = 0;
+            int maxNum = 0;
+
+            candles.Sort();
+            candles.Reverse();
+
+            maxNum = candles[0];
+
+            foreach (var candle in candles)
+            {
+                if (candle >= maxNum)
+                {
+                    result++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = Result.birthdayCakeCandles(candles);
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Problem-Solving-Easy-Difficulty/Diagonal Difference/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Diagonal_Difference
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+
+    //https://www.hackerrank.com/challenges/diagonal-difference/problem
+    class Result
+    {
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int result = 0;
+            int firstSum = 0;
+            int secondSum = 0;
+
+
+            int[,] matrixArr = new int[arr.Count, arr.Count];
+            int[] tempArr = new int[arr.Count];
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                tempArr = arr[i].ToArray();
+
+                for (int j = 0; j < arr.Count; j++)
+                {
+                    matrixArr[i, j] = tempArr[j];
+                }
+            }
+
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                firstSum += matrixArr[i, i];
+            }
+
+            for (int i = 0, j = arr.Count - 1; i < arr.Count; i++, j--)
+            {
+                secondSum += matrixArr[i, j];
+            }
+
+
+            result = Math.Abs(firstSum - secondSum);
+
+            return result;
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = Result.diagonalDifference(arr);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
