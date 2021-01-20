1f589628626300645bf5a746980e3f5e4ce5bfe4
beshkata <beshkata141@gmail.com>
2021-01-20 16:17:15 +0200

    Added GradingStudents and MiniMaxSum

---
+++ Problem-Solving-Easy-Difficulty/GradingStudents/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradingStudents
+{
+    //https://www.hackerrank.com/challenges/grading/problem
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = Result.GradingStudents(grades);
+
+            Console.WriteLine(String.Join("\n", result));
+
+        }
+    }
+    class Result
+    { 
+        public static List<int> GradingStudents(List<int> grades)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                if (grades[i] > 37)
+                {
+                    if (grades[i] % 10 == 3 || grades[i] % 10 == 8)
+                    {
+                        result.Add(grades[i] + 2);
+                    }
+                    else if (grades[i] % 10 == 4 || grades[i] % 10 == 9)
+                    {
+                        result.Add(grades[i] + 1);
+                    }
+                    else
+                    {
+                        result.Add(grades[i]);
+                    }
+
+                }
+                else
+                {
+                    result.Add(grades[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Problem-Solving-Easy-Difficulty/MiniMaxSum/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MiniMaxSum
+{
+    //https://www.hackerrank.com/challenges/mini-max-sum/problem
+    class Program
+    {
+        static void miniMaxSum(int[] arr)
+        {
+            int maxNumIndex = 0;
+            int minNumIndex = 0;
+
+            ulong maxSum = 0;
+            ulong minSum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > arr[maxNumIndex])
+                {
+                    maxNumIndex = i;
+                }
+                if (arr[i] < arr[minNumIndex])
+                {
+                    minNumIndex = i;
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != maxNumIndex)
+                {
+                    minSum += (ulong)arr[i];
+                }
+                if (i != minNumIndex)
+                {
+                    maxSum += (ulong)arr[i];
+                }
+            }
+
+            Console.WriteLine("{0} {1}", minSum, maxSum);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            miniMaxSum(arr);
+        }
+    }
+}
