c0620a85af1c3e6ff020c4fc8b8a8e1048c36530
beshkata <beshkata141@gmail.com>
2021-04-27 16:07:21 +0300

    Added Problem-Solving-Medium-Difficulty.sln with two tasks solved. ClimbingLeaderboard task with 4 tests time out

---
+++ Problem-Solving-Medium-Difficulty/ClimbingLeaderboard/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClimbingLeaderboard
+{
+    //https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
+
+    class Player
+    {
+        public Player(int score)
+        {
+            Score = score;
+        }
+        public int Score { get; set; }
+
+        public int Rank { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+            int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+            List<int> result = ClimbingLeaderboard(ranked, player);
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+            
+        }
+
+        private static List<int> ClimbingLeaderboard(List<int> ranked, List<int> player)
+        {
+            List<Player> players = new List<Player>(ranked.Count + 1);
+
+            for (int i = 0; i < ranked.Count; i++)
+            {
+                Player player1 = new Player(ranked[i]);
+                players.Add(player1);
+            }
+
+            players = players.OrderByDescending(x => x.Score).ToList();
+            DenseRanking(players, 0);
+
+            Player newPlayer = new Player(0);
+            players.Add(newPlayer);
+
+            List<int> rankingResult = new List<int>(player.Count);
+
+            for (int i = 0; i < player.Count; i++)
+            {
+                int newPlayerIndex = players.IndexOf(newPlayer);
+
+                players[newPlayerIndex].Score = player[i];
+                players = players.OrderByDescending(x => x.Score).ToList();
+                newPlayerIndex = players.IndexOf(newPlayer);
+
+                DenseRanking(players, newPlayerIndex);
+
+                rankingResult.Add(players[newPlayerIndex].Rank);
+            }
+
+            return rankingResult;
+        }
+
+        private static void DenseRanking(List<Player> players, int index)
+        {
+            int currentRank = 1;
+            if (index > 0)
+            {
+                currentRank = players[index - 1].Rank;
+            }
+
+
+            for (int i = index; i < players.Count; i++)
+            {
+                if (i == 0)
+                {
+                    players[i].Rank = currentRank;
+                }
+                else
+                {
+                    if (players[i].Score == players[i - 1].Score)
+                    {
+                        players[i].Rank = players[i - 1].Rank;
+                    }
+                    else
+                    {
+                        currentRank++;
+                        players[i].Rank = currentRank;
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ Problem-Solving-Medium-Difficulty/MagicSquare/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MagicSquare
+{
+    //https://www.hackerrank.com/challenges/magic-square-forming/problem
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> s = new List<List<int>>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                s.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList());
+            }
+
+            int result = FormingMagicSquare(s);
+            Console.WriteLine(result);
+        }
+
+        private static int FormingMagicSquare(List<List<int>> s)
+        {
+            int[,] one = {
+                {8, 1, 6},
+                {3, 5, 7},
+                {4, 9, 2}
+            };
+            int[,] two = {
+                {4, 3, 8},
+                {9, 5, 1},
+                {2, 7, 6}
+            };
+
+            int[,] three = {
+                {2, 9, 4},
+                {7, 5, 3},
+                {6, 1, 8}
+            };
+            int[,] four = {
+                {6, 7, 2},
+                {1, 5, 9},
+                {8, 3, 4}
+            };
+            int[,] five = {
+                {6, 1, 8},
+                {7, 5, 3},
+                {2, 9, 4}
+            }; 
+            int[,] six = {
+                {8, 3, 4},
+                {1, 5, 9},
+                {6, 7, 2}
+            };
+            int[,] seven = {
+                {4, 9, 2},
+                {3, 5, 7},
+                {8, 1, 6}
+            };
+            int[,] eight = {
+                {2, 7, 6},
+                {9, 5, 1},
+                {4, 3, 8}
+            };
+
+            List<int[,]> matrixes = new List<int[,]>();
+            matrixes.Add(one);
+            matrixes.Add(two);
+            matrixes.Add(three);
+            matrixes.Add(four);
+            matrixes.Add(five);
+            matrixes.Add(six);
+            matrixes.Add(seven);
+            matrixes.Add(eight);
+
+            int cost = int.MaxValue;
+
+            for (int i = 0; i < matrixes.Count; i++)
+            {
+                int currentCost = FindCost(matrixes[i], s);
+
+                if (currentCost < cost)
+                {
+                    cost = currentCost;
+                }
+            }
+            
+
+            return cost;
+        }
+
+        private static int FindCost(int[,] matrix, List<List<int>> s)
+        {
+            int cost = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    if (matrix[row,col] != s[row][col])
+                    {
+                        cost += Math.Abs(matrix[row, col] - s[row][col]);
+                    }
+                }
+            }
+            return cost;
+        }
+    }
+}
