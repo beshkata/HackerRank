5b2074748fb3e8b9b8add8a91a1071c8fbd8b8a5
beshkata <beshkata141@gmail.com>
2021-06-04 13:36:51 +0300

    ClimbingLeaderboard added

--- Problem-Solving-Medium-Difficulty/ClimbingLeaderboard/Program.cs
+++ Problem-Solving-Medium-Difficulty/ClimbingLeaderboard/Program.cs
@@ -6,18 +6,6 @@ namespace ClimbingLeaderboard
 {
     //https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
 
-    class Player
-    {
-        public Player(int score)
-        {
-            Score = score;
-        }
-        public int Score { get; set; }
-
-        public int Rank { get; set; }
-
-    }
-
     class Program
     {
         static void Main(string[] args)
@@ -32,76 +20,62 @@ namespace ClimbingLeaderboard
 
             List<int> result = ClimbingLeaderboard(ranked, player);
 
-            for (int i = 0; i < result.Count; i++)
-            {
-                Console.WriteLine(result[i]);
-            }
             
         }
 
         private static List<int> ClimbingLeaderboard(List<int> ranked, List<int> player)
         {
-            List<Player> players = new List<Player>(ranked.Count + 1);
+            int[] leaderBoard = new int[ranked.Count];
+            int rank = 1;
+            List<int> playerRanking = new List<int>();
 
-            for (int i = 0; i < ranked.Count; i++)
-            {
-                Player player1 = new Player(ranked[i]);
-                players.Add(player1);
-            }
-
-            players = players.OrderByDescending(x => x.Score).ToList();
-            DenseRanking(players, 0);
-
-            Player newPlayer = new Player(0);
-            players.Add(newPlayer);
-
-            List<int> rankingResult = new List<int>(player.Count);
-
-            for (int i = 0; i < player.Count; i++)
-            {
-                int newPlayerIndex = players.IndexOf(newPlayer);
-
-                players[newPlayerIndex].Score = player[i];
-                players = players.OrderByDescending(x => x.Score).ToList();
-                newPlayerIndex = players.IndexOf(newPlayer);
-
-                DenseRanking(players, newPlayerIndex);
-
-                rankingResult.Add(players[newPlayerIndex].Rank);
-            }
-
-            return rankingResult;
-        }
-
-        private static void DenseRanking(List<Player> players, int index)
-        {
-            int currentRank = 1;
-            if (index > 0)
-            {
-                currentRank = players[index - 1].Rank;
-            }
-
-
-            for (int i = index; i < players.Count; i++)
+            for (int i = 0; i < leaderBoard.Length; i++)
             {
                 if (i == 0)
                 {
-                    players[i].Rank = currentRank;
+                    leaderBoard[i] = rank;
                 }
-                else
+                else if (ranked[i] == ranked [i-1])
                 {
-                    if (players[i].Score == players[i - 1].Score)
-                    {
-                        players[i].Rank = players[i - 1].Rank;
-                    }
-                    else
-                    {
-                        currentRank++;
-                        players[i].Rank = currentRank;
-                    }
-
+                    leaderBoard[i] = leaderBoard[i - 1];
+                }
+                else if (ranked[i] < ranked[i - 1])
+                {
+                    rank++;
+                    leaderBoard[i] = rank;
                 }
             }
+
+            for (int i = 0; i < player.Count; i++)
+            {
+                int currentPlayerScore = player[i];
+                int playerRank = 0;
+
+                if (currentPlayerScore >= ranked[0])
+                {
+                    playerRank = leaderBoard[0];
+                    playerRanking.Add(playerRank);
+                    continue;
+                }
+                if (currentPlayerScore < ranked[ranked.Count - 1])
+                {
+                    playerRank = leaderBoard[ranked.Count - 1]++;
+                    playerRanking.Add(playerRank);
+                    continue;
+                }
+
+                while (true)
+                {
+                    if (currentPlayerScore <= ranked[ranked.Count/2])
+                    {
+
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", leaderBoard));
+            return playerRanking;
         }
+
     }
 }
